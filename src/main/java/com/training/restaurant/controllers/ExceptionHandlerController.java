b13f32d53679c4378a55a05ba2e61bdd244c7214
Manuel Aguilera <102045443+aguileraDev@users.noreply.github.com>
2025-01-10 02:47:51 -0400

    test: add ExceptionHandlerControllerTest.java

--- src/main/java/com/training/restaurant/controllers/ExceptionHandlerController.java
+++ src/main/java/com/training/restaurant/controllers/ExceptionHandlerController.java
@@ -5,7 +5,6 @@ import com.training.restaurant.utils.exceptions.ExceptionResponse;
 import com.training.restaurant.utils.exceptions.NotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,13 +14,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionHandlerController {
 
-    private ExceptionResponse exceptionResponse;
+    private final ExceptionResponse exceptionResponse = new ExceptionResponse();
     private static final Logger logger = LoggerFactory.getLogger(ExceptionHandlerController.class);
 
-    @Autowired
-    public ExceptionHandlerController(ExceptionResponse exceptionResponse) {
-        this.exceptionResponse = exceptionResponse;
-    }
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
---
+++ src/test/java/com/training/restaurant/controllers/ExceptionHandlerControllerTest.java
@@ -0,0 +1,45 @@
+package com.training.restaurant.controllers;
+
+import com.training.restaurant.utils.exceptions.BadRequestException;
+import com.training.restaurant.utils.exceptions.ExceptionResponse;
+import com.training.restaurant.utils.exceptions.NotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class ExceptionHandlerControllerTest {
+
+    private ExceptionResponse exceptionResponse;
+
+    @InjectMocks
+    private ExceptionHandlerController exceptionHandlerController;
+
+    @BeforeEach
+    public void setUp() {
+        exceptionResponse = new ExceptionResponse();
+    }
+    @Test
+    @DisplayName("Should be able to handle not found exception")
+    public void testHandleNotFoundException() {
+        NotFoundException notFoundException = new NotFoundException("Error Message");
+        ResponseEntity<Void> response = exceptionHandlerController.handleNotFoundException(notFoundException);
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Should be able to handle bad request exception")
+    public void testHandleBadRequestException() {
+        BadRequestException badRequestException = new BadRequestException("Error message");
+        ResponseEntity<ExceptionResponse> response = exceptionHandlerController.handleBadRequestException(badRequestException);
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+
+    }
+}
